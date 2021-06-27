b7da703738e50a31c11028d4cfe5bd6c06a45d04
wan17002 <wan17002@byui.edu>
2021-06-26 23:21:26 -0600

    preston-10

---
+++ Lesson-10/weather-site/preston/js/script.js
@@ -0,0 +1,123 @@
+window.addEventListener('load', () =>{
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+    
+    findTime();
+    
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+});
+//Wednesday 24, July 2020
+
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=420789f3ef80300ac1d6280bc39de5ca&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    let currentWeather;
+
+    document.querySelector("#weatherCondition").textContent = jsObject.list[0].weather[0].main;
+    document.querySelector("#temperature").textContent = Math.round(jsObject.list[0].main.temp_max) + "F";
+    document.querySelector("#humidity").textContent = jsObject.list[0].main.humidity + "%";
+    document.querySelector("#windSpeed").textContent = jsObject.list[0].wind.speed + "mph";
+
+    for (let day = 0; day < 5; day++){
+        document.querySelector("#forecastImagesDiv").children[day].firstChild.innerHTML = jsObject.list[day].main.temp + "&#176;F";
+    }
+
+
+      });
+
+
+function findTime(){
+    const currentYear = new Date().getFullYear();
+    const currentDay = new Date().getDate();
+    let currentDayofWeek = findCurrentDayofWeek();
+    let currentMonth = findCurrentDayofMonth();
+
+    let myElement = document.querySelector("#announcementBar");
+    
+    if (currentDayofWeek == "Friday"){
+        myElement.style.display = "block";
+    }else{
+        myElement.style.display = "none";
+    }
+    
+    document.getElementById('inputCurrentYear').innerHTML = currentYear;
+    document.getElementById('inputCurrentDate').innerHTML = currentDayofWeek + " " + currentDay + ", " + currentMonth + " " + currentYear;
+}
+
+function findCurrentDayofWeek(){
+    let currentDayofWeekNum = new Date().getDay();
+    let currentDayofWeek = "";
+    switch(currentDayofWeekNum){
+        case 0:
+            currentDayofWeek = "Sunday";
+            break;
+        case 1:
+            currentDayofWeek = "Monday";
+            break;
+        case 2:
+            currentDayofWeek = "Tuesday";
+            break;
+        case 3:
+            currentDayofWeek = "Wednesday";
+            break;
+        case 4:
+            currentDayofWeek = "Thursday";
+            break;
+        case 5:
+            currentDayofWeek = "Friday";
+            break;
+        case 6:
+            currentDayofWeek = "Saturday";
+            break;
+    }
+    return currentDayofWeek;
+}
+
+function findCurrentDayofMonth(){
+    let currentDayofMonthNum = new Date().getMonth();
+    let currentDayofMonth ="";
+    switch(currentDayofMonthNum){
+        case 0:
+            currentDayofMonth = "January";
+            break;
+        case 1:
+            currentDayofMonth = "February";
+            break;
+        case 2:
+            currentDayofMonth = "March";
+            break;
+        case 3:
+            currentDayofMonth = "April";
+            break;
+        case 4:
+            currentDayofMonth = "May";
+            break;
+        case 5:
+            currentDayofMonth = "June";
+            break;
+        case 6:
+            currentDayofMonth = "July";
+            break;
+        case 7:
+            currentDayofMonth = "August";
+            break;
+        case 8:
+            currentDayofMonth = "September";
+            break;
+        case 9:
+            currentDayofMonth = "October";
+            break;
+        case 10:
+            currentDayofMonth = "November";
+            break;
+        case 11:
+            currentDayofMonth = "December";
+            break;
+    }
+    return currentDayofMonth;
+}
+
---
+++ Lesson-10/weather-site/preston/js/windchill.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () =>{
+    var temperature = document.querySelector("#temperature").textContent;
+    var windSpeed = document.querySelector('#windSpeed').textContent;
+
+    
+    
+    if (temperature <= 50 && windSpeed > 3){
+        var calculateChill = Math.ceil(35.74 + (0.6215 * temperature) - (35.75 * (Math.pow(windSpeed,0.16))) + (0.4275 * temperature * (Math.pow(windSpeed,0.16))));
+        document.getElementById("windChill").innerHTML = calculateChill;
+        document.getElementById('tempSymbol').innerHTML = "&#176;F";
+        
+        tempSymbol.innerHTML = "&#176;";
+    }else{
+        document.getElementById("windChill").innerHTML = "N/A";
+    }
+});

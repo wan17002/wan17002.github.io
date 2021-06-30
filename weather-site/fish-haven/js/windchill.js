b80e25c39925b07eaef9fc5ede720fda26e1a976
wan17002 <wan17002@byui.edu>
2021-06-30 12:05:57 -0600

    commit

---
+++ weather-site/fish-haven/js/windchill.js
@@ -0,0 +1,46 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=420789f3ef80300ac1d6280bc39de5ca&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let filteredList = jsObject.list.filter(time => time.dt_txt.includes('18:00:00'))
+    console.log(filteredList);
+    let currentWeather;
+    const currentWeekDay = new Date().getDay();
+    let currentDay;
+    let resetDay = 0;
+    document.querySelector("#weatherCondition").textContent = jsObject.list[0].weather[0].main;
+    document.querySelector("#temperature").textContent = Math.round(jsObject.list[0].main.temp_max) + "F";
+    document.querySelector("#humidity").textContent = jsObject.list[0].main.humidity + "%";
+    document.querySelector("#windSpeed").textContent = Math.round(jsObject.list[0].wind.speed) + "mph";
+    let iconURL = "http://openweathermap.org/img/wn/";
+    for (let day = 0; day < filteredList.length; day++){
+        if (currentWeekDay + day <= 6){
+            currentDay = findCurrentDayofWeek(currentWeekDay + day);
+        }else{
+            currentDay = findCurrentDayofWeek(resetDay);
+            resetDay += 1;
+        }
+        document.querySelector("#forecastImagesDiv").children[day].firstChild.textContent = currentDay;
+        document.querySelector("#forecastImagesDiv").children[day].children[1].innerHTML = Math.round(filteredList[day].main.temp) + "&#176;F";
+        document.querySelector("#forecastImagesDiv").children[day].children[2].setAttribute('src', iconURL + filteredList[day].weather[0].icon + ".png");
+        document.querySelector("#forecastImagesDiv").children[day].children[2].setAttribute('alt', filteredList[day].weather[0].description);
+    }
+});
+
+window.addEventListener('load', () =>{
+    let temperature = document.querySelector("#temperature").textContent;
+    let windSpeed = document.querySelector('#windSpeed').textContent;
+
+    
+    
+    if (temperature <= 50 && windSpeed > 3){
+        let calculateChill = Math.ceil(35.74 + (0.6215 * temperature) - (35.75 * (Math.pow(windSpeed,0.16))) + (0.4275 * temperature * (Math.pow(windSpeed,0.16))));
+        document.getElementById("windChill").innerHTML = calculateChill;
+        document.getElementById('tempSymbol').innerHTML = "&#176;F";
+        
+        tempSymbol.innerHTML = "&#176;";
+    }else{
+        document.getElementById("windChill").innerHTML = "N/A";
+    }
+});
---
+++ weather-site/home-page/js/town-data.js
@@ -0,0 +1,78 @@
+async function getTown(){
+    const url = "https://byui-cit230.github.io/weather/data/towndata.json";
+    
+    const response = await fetch(url);
+
+    if(response.status==200){
+        return response.json();
+    }
+    else {
+        throw new Error("No meals found" + response.status);
+    }
+}
+
+function newTown(){
+    const main = document.getElementsByTagName('main');
+
+    const town = getTown()
+    .then(function(data){
+        console.log(data);
+
+        new_town = data['towns'];
+
+        
+
+        //Fish Haven, Soda Springs, and Preston
+        for (var i = 0; i < new_town.length ; i++){
+            if(new_town[i].name == "Fish Haven" || new_town[i].name == "Preston" || new_town[i].name == "Soda Springs"){
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let italics = document.createElement('i');
+                let yearFounded = document.createElement('p');
+                let currentPopulation = document.createElement('p');
+                let rainFall = document.createElement('p');
+                let townPhoto = document.createElement('img');
+                let textDiv = document.createElement('div');
+                document.querySelector('main').appendChild(card);
+
+                h2.textContent = new_town[i].name;
+                italics.textContent = new_town[i].motto;
+                yearFounded.textContent = "Year Founded: " + new_town[i].yearFounded;
+                currentPopulation.textContent = "Population: " + new_town[i].currentPopulation;
+                rainFall.textContent = "Annual Rain Fall: " + new_town[i].averageRainfall;
+                
+                switch(new_town[i].name){
+                    case "Soda Springs":
+                        townPhoto.setAttribute('src', 'images/soda-springs_250x166.jpg');
+                        townPhoto.setAttribute('alt', new_town[i].name);
+                        break;
+                    case "Preston":
+                        townPhoto.setAttribute('src', 'images/preston_250x166.jpg');
+                        townPhoto.setAttribute('alt', new_town[i].name);
+                        break;
+                    case "Fish Haven":
+                        townPhoto.setAttribute('src', 'images/fish-haven_250x166.jpg');
+                        townPhoto.setAttribute('alt', new_town[i].name);
+                        break;
+                }
+                textDiv.appendChild(h2);
+                textDiv.appendChild(italics);
+                textDiv.appendChild(yearFounded);
+                textDiv.appendChild(currentPopulation);
+                textDiv.appendChild(rainFall);
+                card.appendChild(textDiv);
+                card.appendChild(townPhoto);
+
+
+                
+            }
+            
+        }
+    });
+}
+window.addEventListener('load',(event)=>{
+    newTown();
+});
+
+
+    

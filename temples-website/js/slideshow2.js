3078d9b68355af3bc70b742f3ba316738ab18e76
wan17002 <wan17002@byui.edu>
2021-07-09 19:21:32 -0600

    commit

---
+++ temples-website/js/slideshow2.js
@@ -0,0 +1,29 @@
+var slideIndex = 1;
+window.addEventListener('load', () =>{
+
+  showSlides2(slideIndex);
+});
+
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active2";
+}
